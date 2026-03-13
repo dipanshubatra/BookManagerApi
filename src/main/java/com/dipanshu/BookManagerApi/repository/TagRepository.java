e275227a16b5762def3bbf1163a618c3907fe025
Dipanshu Batra <devbatra887@gmail.com>
2026-03-13 09:41:31 +0530

    feat: implement tag system with Bookmark–Tag many-to-many mapping and tag resolution logic

--- src/main/java/com/dipanshu/BookManagerApi/repository/TagRepository.java
+++ src/main/java/com/dipanshu/BookManagerApi/repository/TagRepository.java
@@ -1,4 +1,10 @@
 package com.dipanshu.BookManagerApi.repository;
 
-public class TagRepository {
+import com.dipanshu.BookManagerApi.entity.Tag;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface TagRepository extends JpaRepository<Tag,Long> {
+    Optional<Tag> findByName(String name);
 }
--- src/main/java/com/dipanshu/BookManagerApi/service/BookmarkService.java
+++ src/main/java/com/dipanshu/BookManagerApi/service/BookmarkService.java
@@ -1,19 +1,31 @@
 package com.dipanshu.BookManagerApi.service;
 
 import com.dipanshu.BookManagerApi.entity.Bookmark;
+import com.dipanshu.BookManagerApi.entity.Tag;
 import com.dipanshu.BookManagerApi.repository.BookmarkRepository;
+import com.dipanshu.BookManagerApi.repository.TagRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.HashSet;
 import java.util.List;
 import java.util.Optional;
+import java.util.Set;
 
 @Service
 public class BookmarkService {
     private final BookmarkRepository bookmarkRepository;
-    public BookmarkService(BookmarkRepository bookmarkRepository){
+    private final TagRepository tagRepository;
+
+
+    public BookmarkService(BookmarkRepository bookmarkRepository, TagRepository tagRepository){
         this.bookmarkRepository= bookmarkRepository;
+        this.tagRepository = tagRepository;
     }
     public Bookmark createBookmark(Bookmark bookmark){
+        List<String> tagNames = bookmark.getTags().stream().map(Tag::getName).toList();
+        Set<Tag> resolvedTags = resolveTags(tagNames);
+        bookmark.setTags(resolvedTags);
+        
         return bookmarkRepository.save(bookmark);
     }
     public List<Bookmark> findAllBookmarks(){
@@ -55,6 +67,22 @@ public class BookmarkService {
         }
 
         return Optional.empty();
+    }
+    private Set<Tag> resolveTags(List<String> tagNames){
+        Set<Tag>tags = new HashSet<>();
+        for(String tagName :tagNames){
+            Optional<Tag> existingTag = tagRepository.findByName(tagName);
+            if(existingTag.isPresent()){
+                tags.add(existingTag.get());
+            }
+            else{
+                Tag newTag = new Tag();
+                newTag.setName(tagName);
+                Tag savedTag = tagRepository.save(newTag);
+                tags.add(savedTag);
+            }
+        }
+        return tags;
     }
 }
 
