f9557b0a9e9c79cbc128be1aaf74702674672efc
Dipanshu Batra <devbatra887@gmail.com>
2026-03-28 11:46:18 +0530

    feat: docker configured

--- src/main/java/com/dipanshu/BookManagerApi/config/WebConfig.java
+++ src/main/java/com/dipanshu/BookManagerApi/config/WebConfig.java
@@ -14,7 +14,7 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173") // frontend
+                        .allowedOrigins("http://localhost:5173","https://bookmark-apifrontend.vercel.app") // frontend
                         .allowedMethods("*")
                         .allowedHeaders("*")
                         .allowCredentials(true);
